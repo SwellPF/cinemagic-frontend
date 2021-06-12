385a928c83517cf48ce02ba61856f92bee39f69a
Paul Fell <swellpf@gmail.com>
2021-06-12 14:30:54 -0400

    Configuring App, index, MoviesContainer

---
+++ src/containers/MoviesContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import {fetchMovies} from '../actions/fetchMovies';
+import Movies from '../components/Movies';
+import Movie from '../components/Movie';
+import MovieInput from '../components/MovieInput';
+import NavBar from '../components/NavBar';
+
+class MoviesContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchMovies()
+    }
+
+    render () {
+        return (
+            <div>
+                <NavBar/>
+                <Switch>
+                    <Route path='/movies/new' component={MovieInput}/>
+                    <Route path='/movies/:id' render={(routerProps) => <Movie {...routerProps} movies={this.props.movies}/>}/>
+                    <Route path='/movies' render={(routerProps) => <Movies {...routerProps} movies={this.props.movies}/>}/>
+                </Switch>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        movies: state.movies
+    }
+}
+
+
+export default connect(mapStateToProps, {fetchMovies})(MoviesContainer)
--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {BrowserRouter as Router} from 'react-router-dom';
+import movieReducer from './reducers/movieReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(movieReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
