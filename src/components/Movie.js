8ad1bd5c5cd89e23ac20f1b9521e48a614cd4ceb
Paul Fell <swellpf@gmail.com>
2021-06-13 14:55:02 -0400

    Converted Movie and Movies to functional components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import CommentsContainer from '../containers/CommentsContainer';
 
-class Movie extends React.Component {
+const Movie = (props) => {
+    console.log(props)
+    let movie = props.movies.filter(movie => movie.id == props.match.params.id)[0]
+    console.log(movie)
 
-    render() {
-        const {movie} = this.props;
-
-        return(
-            <div>
-            <li>{movie.title}
-            <CommentsContainer /></li>
-            </div>
-        );
-    }
-;}
+    return (
+        <div>
+            {movie ? movie.title : null}
+            <CommentsContainer movie={movie}/>
+        </div>
+    )
+}
 
 export default Movie
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,23 +2,15 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Movie from './Movie'
 
-class Movies extends React.Component {
-    render() {
-        const {movies} = this.props;
-        const movieList = movies.map(movie => {
-            return (
-                <Movie
-                    key={movie.id}
-                    movie={movie}
-                    />
-            )
-        });
-        return (
-            <ul>
-                {movieList}
-            </ul>
-        )
-    }
-};
+const Movies = (props) => {
+    return (
+        <div>
+            {props.movies.map(movie =>
+                <li key={movie.id}>
+                    <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>)}
+        </div>
+    )
+}
 
 export default Movies
