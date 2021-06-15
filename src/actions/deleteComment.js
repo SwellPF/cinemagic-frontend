eb186d641ba87e8ac14786b9d182c0cefd14b6e0
Paul Fell <swellpf@gmail.com>
2021-06-15 16:02:45 -0400

    Updates to deleteComment action

--- src/actions/deleteComment.js
+++ src/actions/deleteComment.js
@@ -1,9 +1,13 @@
 export const deleteComment = (commentId, movieId) => {
-    return (dispatch) => {
-        fetch(`http://localhost:3000/api/v1/movies/${movieId}/comments/${commentId}`, {
-            method: 'DELETE',
-        })
-        .then(response => response.json())
-        .then(movie => dispatch({type: 'DELETE_COMMENT', payload: movie}))
-    }
-}
+
+return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/comments`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: 'DELETE',
+        body: JSON.stringify(content)
+    })
+    .then(response => response.json())
+    .then(movie => dispatch({type: 'DELETE_COMMENT', payload: content}))
