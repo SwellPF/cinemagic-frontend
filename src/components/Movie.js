39a92cf1ef18ab547614f12b012341903a7ad221
Paul Fell <swellpf@gmail.com>
2021-06-22 21:13:12 -0400

    Updates to Movie.js to implement css formatting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,16 +10,18 @@ const Movie = (props) => {
     
     // debugger
     return (
-        <div>   
-            <div className='content-center'><img src={movie.image_link} alt='movie-poster'/></div><br/>
-            <h2 className='text-left text-blue-700 font-bold text-3xl'>{movie.title}</h2>
-            <h4><b>Description:</b></h4> {movie.description}
-            <p><b>Genre:</b> {movie.genre_name}</p>
-            <p><b>Year Released:</b> {movie.year_released}</p>
-            <b>Comments: </b>({movie.comments.length ? movie.comments.length : "None yet. Be the first to comment on this movie!"})<br/><br/>        
-            <Link to={`/movies/${movie.id}/edit`}> <button className="rounded-md border border-blue-700 hover:bg-blue-500 py-.5 px-4">Edit Movie</button></Link><br/><br/>
+        <div><div class="grid grid-cols-2 gap-4">   
+            <div className='w-1/4'><img src={movie.image_link} alt='movie-poster'/></div>
+            <div className='w-3/4'>
+                <h2 className='text-left text-blue-700 font-bold text-3xl'>{movie.title}</h2>
+                <h4 className='text-left'><b>Description:</b></h4> <p className='text-left'>{movie.description}</p>
+                <p className='text-left'><b>Genre:</b> {movie.genre_name}</p>
+                <p className='text-left'><b>Year Released:</b> {movie.year_released}</p>
+                <b>Comments: </b>({movie.comments.length ? movie.comments.length : "None yet. Be the first to comment on this movie!"})<br/><br/>        
+            <Link to={`/movies/${movie.id}/edit`}> <button className="rounded-md border border-blue-700 hover:bg-blue-500 py-.5 px-4">Edit Movie</button></Link></div><br/>
+        </div><hr/><br/>
         <CommentsContainer movie={movie}/>
-        </div>
+    </div>
     )
 }
 
